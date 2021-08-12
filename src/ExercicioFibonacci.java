3c4dd5c7aafac680f71cf08b2d379849d3864025
Lucas da Silva Potrich <silva.lucasrs@gmail.com>
2021-08-12 15:31:38 -0300

    Exercício Fibonacci

--- src/ExercicioFibonacci.java
+++ src/ExercicioFibonacci.java
@@ -1,9 +1,25 @@
+import java.util.Scanner;
 
 public class ExercicioFibonacci {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner leitor = new Scanner (System.in);
+		int n, ant1, ant2, atual;
+		
+		ant2 = 1;
+		atual = 0;
+		
+		System.out.println("Quantos elementos você quer exibir? " );
+		n = leitor.nextInt();
+		
+		for (int i=0; i<n; i++) {
+			ant1=ant2;
+			ant2= atual;
+			atual = ant1 + ant2;
+			System.out.println(atual);	
+			
+		}
+			
 	}
 
 }
