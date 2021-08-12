5d94f1da07e1d5a16c6d6d283aa670712bfafab2
Lucas da Silva Potrich <silva.lucasrs@gmail.com>
2021-08-12 14:59:53 -0300

    exercício10

--- src/Ex10.java
+++ src/Ex10.java
@@ -1,9 +1,24 @@
+import java.util.Scanner;
 
 public class Ex10 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner
+		leitor = new Scanner(System.in);
+		String nome, setor;
+		int meioTransporte, placa;
+		
+		for (int i=1; i<=150; i++) {
+			System.out.println("Digite o nome do funcionário: ");
+			nome = leitor.nextLine();
+			System.out.println("Digite o setor do funcionário: ");
+			setor = leitor.nextLine();
+			System.out.println("Digite o meio de transporte: 1 - Carro ou 2 - Õnibus ");
+			meioTransporte = leitor.nextInt();
+			leitor.nextLine(); // gambiarra para consumir enter na fila pra zerar e não dar buffer.
+			
+			
+		}
 	}
 
 }
