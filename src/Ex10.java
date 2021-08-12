e162d4f3d235840ec3f3b2c31b1de900a0b03f5e
Lucas da Silva Potrich <silva.lucasrs@gmail.com>
2021-08-12 15:12:06 -0300

    terminado o exercício 10

--- src/Ex10.java
+++ src/Ex10.java
@@ -16,9 +16,51 @@ public class Ex10 {
 			System.out.println("Digite o meio de transporte: 1 - Carro ou 2 - Õnibus ");
 			meioTransporte = leitor.nextInt();
 			leitor.nextLine(); // gambiarra para consumir enter na fila pra zerar e não dar buffer.
-			
+			switch (meioTransporte) {
+			case 1:
+				System.out.println("Informe o último número da sua placa:  ");
+				placa = leitor.nextInt();
+				leitor.nextLine();
+				switch (placa) {
+				case 1:
+				case 2:
+					System.out.println("Segunda Dia do funcionário:  " +nome + "  do setor " + setor);
+					System.out.println("----------------------------------------------------------" );
+				case 3:
+				case 4:
+					System.out.println("Terça Dia do funcionário:  " +nome + "  do setor " + setor);
+					System.out.println("----------------------------------------------------------" );
+					break;
+				case 5:
+				case 6:
+					System.out.println("Quarta Dia do funcionário:  " +nome + "  do setor " + setor);
+					System.out.println("----------------------------------------------------------" );
+					break;
+				case 7:
+				case 8:
+					System.out.println("Quinta Dia do funcionário:  " +nome + "  do setor " + setor);
+					System.out.println("----------------------------------------------------------" );
+					break;
+				case 9:
+				case 0:
+					System.out.println("Sexta Dia do funcionário:  " +nome + "  do setor " + setor);
+					System.out.println("----------------------------------------------------------" );
+					break;
+				default:
+					System.out.println("Informação inválida:  ");
+					break;
+				}	
+				break;
+			case 2:
+				System.out.println("Obrigado por ir de Ônibus! "  +nome + "  do setor " + setor);	
+				break;
+			default:
+			// informa que é inválido
+				System.out.println("Valor digitado inválido");
+			}
 			
 		}
+		leitor.close();
 	}
 
 }
